389782814b2d84d26f11dc482fe609ab083dd660
KeiferJu <jkf19980216@qq.com>
2021-06-17 21:50:45 +0800

    first commit

--- README.md
+++ README.md
@@ -2,4 +2,33 @@
 
 [DEMO](https://keiferju.github.io/css3-tx/)
 
+### 特效
 1. [文字标题](dllcn)
+
+
+### Canvas背景
+1. [AnimatedBackground](canvas/AnimatedBackground)
+
+![](canvas/AnimatedBackground/img/1.gif)
+
+![](canvas/iAnimatedBackground/mg/2.gif)
+
+![](canvas/AnimatedBackground/img/3.gif)
+
+![](canvas/AnimatedBackground/img/4.gif)
+
+2. [3DBackground](canvas/3DBackground)
+
+![](canvas/3DBackground/img/5.gif)
+
+3. [PoppingBubbles](canvas/PoppingBubbles)
+
+![](canvas/PoppingBubbles/img/3-1.gif)
+
+4. [webgl-canvas](canvas/webgl-canvas)
+
+![](canvas/webgl-canvas/webgl-canvas.png)
+
+### loading动画
+
+1. [css3-flower-loading](loading/css3-flower-loading)
--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const HelloVueApp = {
     data() {
         return {
-            lists: [{
+            txlists: [{
                 name: '标题特效',
                 url: 'tx/dllcn/index.html'
+            }],
+            canvaslists: [
+                {
+                    name: 'webglCanvas',
+                    url: 'canvas/webglCanvas/index.html'
+                },
+                {
+                    name: 'AnimatedBackground',
+                    url: 'canvas/AnimatedBackground/index.html'
+                },
+                {
+                    name: 'PoppingBubbles',
+                    url: 'canvas/PoppingBubbles/index.html'
+                },
+                {
+                    name: '3DBackground',
+                    url: 'canvas/3DBackground/index.html'
+                },
+                {
+                    name: '3DBackground2',
+                    url: 'canvas/3DBackground/index2.html'
+                }
+            ],
+            loadinglists: [{
+                name: 'css3-flower-loading',
+                url: 'loading/css3-flower-loading/index.html'
             }],
             pageurl: ""
         }
     },
     created() {
-        this.pageurl = this.lists[0].url
+        this.pageurl = this.canvaslists[0].url
     },
     methods: {
         todoPage(url) {
