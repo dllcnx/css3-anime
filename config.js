71a37baacca69bb964648bc660af6fc411c67e8d
KeiferJu <jkf19980216@qq.com>
2021-06-18 11:19:49 +0800

    更新demo页面

--- config.js
+++ config.js
@@ -1,34 +1,58 @@
-export let config = {
-    txlists: [{
-        name: '标题特效',
-        url: 'tx/dllcn/index.html'
-    }, {
-        name: '404',
-        url: 'tx/404_01/index.html'
-    }],
-    canvaslists: [{
-            name: 'webglCanvas',
-            url: 'canvas/webglCanvas/index.html'
-        },
-        {
-            name: 'AnimatedBackground',
-            url: 'canvas/AnimatedBackground/index.html'
-        },
-        {
-            name: 'PoppingBubbles',
-            url: 'canvas/PoppingBubbles/index.html'
-        },
-        {
-            name: '3DBackground',
-            url: 'canvas/3DBackground/index.html'
-        },
-        {
-            name: '3DBackground2',
-            url: 'canvas/3DBackground/index2.html'
-        }
-    ],
-    loadinglists: [{
-        name: 'css3-flower-loading',
-        url: 'loading/css3-flower-loading/index.html'
-    }]
-}
+export let config = [{
+        title: '特效',
+        data: [{
+            name: '标题特效',
+            url: 'tx/dllcn/index.html'
+        }, {
+            name: '404',
+            url: 'tx/404_01/index.html'
+        }]
+    },
+    {
+        title: 'Canvas背景',
+        data: [{
+                name: 'webglCanvas',
+                url: 'canvas/webglCanvas/index.html'
+            },
+            {
+                name: 'AnimatedBackground',
+                data: [{
+                    name: 'AnimatedBackground',
+                    url: 'canvas/AnimatedBackground/index.html'
+                }, {
+                    name: 'AnimatedBackground2',
+                    url: 'canvas/AnimatedBackground/index2.html'
+                }, {
+                    name: 'AnimatedBackground3',
+                    url: 'canvas/AnimatedBackground/index3.html'
+                }, {
+                    name: 'AnimatedBackground4',
+                    url: 'canvas/AnimatedBackground/index4.html'
+                }]
+            },
+            {
+                name: 'PoppingBubbles',
+                url: 'canvas/PoppingBubbles/index.html'
+            },
+            {
+                name: '3DBackground',
+                data: [{
+                        name: '3DBackground',
+                        url: 'canvas/3DBackground/index.html'
+                    },
+                    {
+                        name: '3DBackground2',
+                        url: 'canvas/3DBackground/index2.html'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'loading动画',
+        data: [{
+            name: 'css3-flower-loading',
+            url: 'loading/css3-flower-loading/index.html'
+        }]
+    },
+]
